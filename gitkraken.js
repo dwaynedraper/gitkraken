71b3a73c38619f36e5a117383c419d8cb185aa6b
Dwayne <deandraper82@gmail.com>
2021-01-08 22:25:11 -0600

    Before branch

--- gitkraken.js
+++ gitkraken.js
@@ -2,3 +2,4 @@ function teachEveryoneGitKraken() {
     console.log('This is how you make your first commit.')
 }
 // This is where I wrote legendary code that makes buttons 4d using nothing but vanilla JS and CSS.
+// Commiting something in master before trying to branch.
